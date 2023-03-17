a75a68d06d8434a916cda13996c0b8163d7ea379
sergio.nieto <sergio.nieto@inegi.org.mx>
2023-03-16 22:15:23 -0600

    SE sube por primera vez

---
+++ aplicación/Model/Info.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carga.Model
+{
+    public class Info
+    {
+        public Header Header { get; set; }
+        public List<Series> Series { get; set; }
+    }
+
+    public class Header {
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+    public class Series
+    {
+        public string Indicador { get; set; }
+        public string Freq { get; set; }
+        public string Topic { get; set; }
+        public string Unit { get; set; }
+        public string Unit_mult { get; set; }
+        public string Note { get; set; }
+        public string Lastupdate { get; set; }
+        public string Source { get; set; }
+        public string Status { get; set; }
+        public List<Observations> Observations { get; set; }
+    }
+
+    public class Observations 
+    {
+        public string Cober_geo { get; set; }
+        public string Obs_exception { get; set; }
+        public string Obs_note { get; set; }
+        public string Obs_source { get; set; }
+        public string Obs_status { get; set; }
+        public string Obs_value { get; set; }
+        public string Time_period { get; set; }
+    }
+}
---
+++ aplicación/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using carga.Model;
+using Newtonsoft.Json;
+
+using Microsoft.Data.Sqlite;
+using System.Net;
+using System.Text;
+
+namespace carga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+            wc.Headers[HttpRequestHeader.ContentType] = "application/json; charset=utf-8";
+            wc.Headers[HttpRequestHeader.UserAgent] = "Desarrolladores_Inegi";
+            wc.Encoding = Encoding.UTF8;
+            //string[] parametros = parameters.Split('*');
+            string token = "970a2694-e217-5311-12b9-b9c1b9403d4c";
+            string ruta = "http://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000002,1002000003,1002000001/es/0700/false/BISE/2.0/"+ token + "?type=json";
+ 
+            var response = wc.UploadString(ruta, "GET");
+
+            SqliteConnection cadenaconexion = new SqliteConnection("Data source= ../../../../../mydatabase.db");
+            cadenaconexion.Open();
+
+            string queryBU="";
+            SqliteCommand comandoBU;
+            SqliteDataReader resultBU;
+            Info dato = JsonConvert.DeserializeObject<Info>(response);
+              foreach (var Ser in dato.Series)
+              {
+                //Buscar unidades iguales
+               queryBU = "SELECT * FROM Unit_catalog WHERE Unit=" + Ser.Unit + ";";
+                comandoBU = new SqliteCommand(queryBU, cadenaconexion);
+                resultBU = comandoBU.ExecuteReader();
+                if (!resultBU.HasRows)
+                {
+                    //insertar unidad
+                    string queryU = "insert into Unit_catalog(Unit) values ('" + Ser.Unit + "')";
+                    SqliteCommand comandoU = new SqliteCommand(queryU, cadenaconexion);
+                    comandoU.ExecuteNonQuery();
+                }
+                //Buscar notas iguales
+                queryBU = "SELECT * FROM Note_catalog WHERE Note=" + Ser.Note + ";";
+                comandoBU = new SqliteCommand(queryBU, cadenaconexion);
+                resultBU = comandoBU.ExecuteReader();
+                if (!resultBU.HasRows)
+                {
+                    //insertar nota
+                    string queryN = "insert into Note_catalog(Note) values ('" + Ser.Note + "')";
+                    SqliteCommand comandoN = new SqliteCommand(queryN, cadenaconexion);
+                    comandoN.ExecuteNonQuery();
+                }
+                string queryS = "insert into Series(Indicador,Freq,Topic,Unit,Unit_mult,Note,Source,Lastupdate,Status) values ('" + Ser.Indicador + "','" + Ser.Freq + "','" + Ser.Topic + "','" + Ser.Unit + "','" + Ser.Unit_mult + "','" + Ser.Note + "','" + Ser.Lastupdate + "','" + Ser.Source + "','" + Ser.Status + "')";
+                SqliteCommand comandoS = new SqliteCommand(queryS, cadenaconexion);
+                comandoS.ExecuteNonQuery();
+
+                //recorrer observations
+                foreach(var obs in Ser.Observations)
+                {
+                    string queryO = "insert into Observations(Indicador,Time_period,Obs_value,Obs_exception,Obs_status,Obs_source,Obs_note,Cober_geo) values ('" + Ser.Indicador + "','" + obs.Time_period + "','" + obs.Obs_value + "','" + obs.Obs_exception + "','" + obs.Obs_status + "','" + obs.Obs_source + "','" + obs.Obs_note + "','" + obs.Cober_geo +"')";
+                    SqliteCommand comandoO = new SqliteCommand(queryO, cadenaconexion);
+                    comandoO.ExecuteNonQuery();
+                }
+             }
+            cadenaconexion.Close();
+
+        }
+    }
+}
